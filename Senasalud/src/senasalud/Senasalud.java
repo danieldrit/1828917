6ee663ca52732b32144edcd66a7d233b762dabe2
Daniel Obregozo <sala@HOR-AULA501-008.unihorizonte.salas>
2019-10-29 15:51:59 -0500

    mi primer commit

---
+++ Senasalud/src/senasalud/Senasalud.java
@@ -0,0 +1,13 @@
+
+package senasalud;
+
+import javax.swing.JOptionPane;
+
+public class Senasalud {
+    
+    public static void main(String[] args) {
+     String nombre=JOptionPane.showInputDialog(null,"Cual es su Nombre");
+     System.out.println("Su nombre es:"+nombre);
+    }
+    
+}
