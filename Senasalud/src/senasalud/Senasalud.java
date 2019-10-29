11fe5fc06c58f6a267f77d9f2e80280314872bd0
Daniel Obregozo <sala@HOR-AULA501-008.unihorizonte.salas>
2019-10-29 16:46:30 -0500

    mi tercer commit

--- Senasalud/src/senasalud/Senasalud.java
+++ Senasalud/src/senasalud/Senasalud.java
@@ -6,15 +6,16 @@ import javax.swing.JOptionPane;
 public class Senasalud {
     
     public static void main(String[] args) {
-     //String nombre=JOptionPane.showInputDialog(null,"Cual es su Nombre");
-    // System.out.println("Su nombre es:"+nombre);
+   String nombre=JOptionPane.showInputDialog(null,"Cual es su Nombre");
+   System.out.println("Su nombre es:"+nombre);
     
-    String Apellido=JOptionPane.showInputDialog(null,"Cual es su Apellido");
-    System.out.println("Su Apellido es:"+Apellido );
+   String Apellido=JOptionPane.showInputDialog(null,"Cual es su Apellido");
+   System.out.println("Su Apellido es:"+Apellido );
     String Edad=JOptionPane.showInputDialog(null,"Cual es su Edad");
     System.out.println("Su Edad es:"+Edad );
     
-    
+    String Hobbie=JOptionPane.showInputDialog(null,"Cual es su Hobbie");
+    System.out.println("Su Hobbie es:"+Hobbie);
     }
     
 }
